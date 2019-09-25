58815e6a24126ce517d6acb089e791dd36411ed7
Kasper Meyer <hi@kaspermeyer.com>
2019-09-25 14:47:54 +0200

    Remove dummy tests

--- test/prawn/component_test.rb
+++ test/prawn/component_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
-class Prawn::ComponentTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Prawn::Component::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
+class Prawn::ComponentTest < Prawn::Component::TestCase
 end

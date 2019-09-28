fc197d9914ab4edad5197758dfec3d22a1893c57
Kasper Meyer <hi@kaspermeyer.com>
2019-09-28 11:10:59 +0200

    Improve readability of component test

--- test/unit/component_test.rb
+++ test/unit/component_test.rb
@@ -16,46 +16,46 @@ class ComponentTest < Prawn::Component::TestCase
   end
 
   test "rendering in a prawn document instance" do
+    class BasicComponent < Prawn::Component
+      template do
+        text "Basic"
+      end
+    end
+
     BasicComponent.new.render_in(@document)
 
     assert_document_includes @document, "Basic"
   end
 
-  test "accessing component instance inside template" do
+  test "accessing component in template" do
+    class ArgumentComponent < Prawn::Component
+      template do |component|
+        text component.text
+      end
+
+      attr_reader :text
+
+      def initialize(text:)
+        @text = text
+      end
+    end
+
     ArgumentComponent.new(text: "Argument").render_in(@document)
 
     assert_document_includes @document, "Argument"
   end
 
-  test "rendering with additional content" do
-    ContentComponent.new.render_in(@document) do
-      "Additional content"
+  test "rendering additional content" do
+    class ContentComponent < Prawn::Component
+      template do |_, content|
+        content.call(self)
+      end
+    end
+
+    ContentComponent.new.render_in(@document) do |pdf|
+      pdf.text "Additional content"
     end
 
     assert_document_includes @document, "Additional content"
-  end
-
-  class BasicComponent < Prawn::Component
-    template do
-      text "Basic"
-    end
-  end
-
-  class ArgumentComponent < Prawn::Component
-    template do |component|
-      text component.text
-    end
-
-    attr_reader :text
-
-    def initialize(text:)
-      @text = text
-    end
-  end
-
-  class ContentComponent < Prawn::Component
-    template do |_, content|
-      text content.call
-    end
   end
 end

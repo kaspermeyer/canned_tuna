483729403c8e3f01ce05f6ff52e6d8f1bd98760b
Kasper Meyer <hi@kaspermeyer.com>
2020-06-02 11:00:11 +0200

    Style

--- lib/prawn/component/content_proxy.rb
+++ lib/prawn/component/content_proxy.rb
@@ -2,14 +2,14 @@ module Prawn
   class Component
     class ContentProxy
       def initialize &block
-        @block   = block || proc {}
+        @block = block || proc {}
         @outlets = {}
 
         instance_exec(self, &@block)
       end
 
       def outlet_for name
-        @outlets[name] or proc {}
+        @outlets[name] || proc {}
       end
 
       def outlet name = :default, &block

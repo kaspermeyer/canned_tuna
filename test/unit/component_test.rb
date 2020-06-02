b382eb9ae3bf9510727254309a2ccfaec208f554
Kasper Meyer <hi@kaspermeyer.com>
2020-06-02 22:10:06 +0200

    Clean up tests

--- test/unit/component_test.rb
+++ test/unit/component_test.rb
@@ -6,42 +6,42 @@ class ComponentTest < Prawn::Component::TestCase
   end
 
   test "defining and accessing the template" do
-    class TemplateComponent < Prawn::Component
-      template do
-        "Inside the template"
-      end
-    end
-
     assert_equal "Inside the template", TemplateComponent.template.call
   end
 
   test "rendering in a prawn document instance" do
-    class BasicComponent < Prawn::Component
-      template do
-        text "Basic"
-      end
-    end
-
     BasicComponent.new.draw_in(@document)
 
     assert_document_includes @document, "Basic"
   end
 
   test "component methods in template" do
-    class ArgumentComponent < Prawn::Component
-      template do
-        text heading
-      end
-
-      attr_reader :heading
-
-      def initialize(heading:)
-        @heading = heading
-      end
-    end
-
     ArgumentComponent.new(heading: "Argument").draw_in(@document)
 
     assert_document_includes @document, "Argument"
+  end
+
+  class TemplateComponent < Prawn::Component
+    template do
+      "Inside the template"
+    end
+  end
+
+  class BasicComponent < Prawn::Component
+    template do
+      text "Basic"
+    end
+  end
+
+  class ArgumentComponent < Prawn::Component
+    template do
+      text heading
+    end
+
+    attr_reader :heading
+
+    def initialize(heading:)
+      @heading = heading
+    end
   end
 end
--- test/unit/outlet_test.rb
+++ test/unit/outlet_test.rb
@@ -1,37 +1,19 @@
 require "test_helper"
 
 class OutletTest < Prawn::Component::TestCase
-  setup do
-    @document = Prawn::Document.new
-  end
-
-  test "rendering additional content with outlet" do
-    class ContentComponent < Prawn::Component
-      template do
-        outlet
-      end
-    end
-
-    @document.instance_exec do
+  test "rendering content with outlet" do
+    document = Prawn::Document.new {
       draw ContentComponent do
         text "Additional content"
       end
-    end
+    }
 
-    assert_document_includes @document, "Additional content"
+    assert_document_includes document, "Additional content"
   end
 
-  test "rendering additional content with multiple outlets" do
-    class OutletComponent < Prawn::Component
-      template do
-        outlet(:header)
-        outlet(:body)
-        outlet(:footer)
-      end
-    end
-
-    @document.instance_exec do
-      draw OutletComponent do |content|
+  test "rendering content with multiple outlets" do
+    document = Prawn::Document.new {
+      draw NamedOutletComponent do |content|
         content.outlet(:header) do
           text "Header"
         end
@@ -44,48 +26,62 @@ class OutletTest < Prawn::Component::TestCase
           text "Footer"
         end
       end
-    end
+    }
 
     ["Header", "Body", "Footer"].each do |outlet_text|
-      assert_document_includes @document, outlet_text
+      assert_document_includes document, outlet_text
     end
   end
 
-  test "rendering outlet with default content" do
-    class DefaultContentComponent < Prawn::Component
-      template do
-        outlet do
-          text "Default content"
-        end
-      end
-    end
-
-    @document.instance_exec do
+  test "outlet with default content" do
+    document = Prawn::Document.new {
       draw DefaultContentComponent
-    end
+    }
 
-    assert_document_includes @document, "Default content"
+    assert_document_includes document, "Default content"
   end
 
-  test "rendering multiple outlets with default content" do
-    class DefaultContentsComponent < Prawn::Component
-      template do
-        outlet(:header) { text "Header" }
-        outlet(:body)
-        outlet(:footer) { text "Footer" }
-      end
-    end
-
-    @document.instance_exec do
+  test "multiple outlets with default content" do
+    document = Prawn::Document.new {
       draw DefaultContentsComponent do |content|
         content.outlet(:body) do
           text "Body"
         end
       end
-    end
+    }
 
     ["Header", "Body", "Footer"].each do |outlet_text|
-      assert_document_includes @document, outlet_text
+      assert_document_includes document, outlet_text
+    end
+  end
+
+  class ContentComponent < Prawn::Component
+    template do
+      outlet
+    end
+  end
+
+  class NamedOutletComponent < Prawn::Component
+    template do
+      outlet(:header)
+      outlet(:body)
+      outlet(:footer)
+    end
+  end
+
+  class DefaultContentComponent < Prawn::Component
+    template do
+      outlet do
+        text "Default content"
+      end
+    end
+  end
+
+  class DefaultContentsComponent < Prawn::Component
+    template do
+      outlet(:header) { text "Header" }
+      outlet(:body)
+      outlet(:footer) { text "Footer" }
     end
   end
 end
